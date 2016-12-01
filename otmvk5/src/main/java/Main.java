785f31b281338ca0591ce0bd4731cfe5fe212d48
Petteri Vikström <petteri.vikstrom@helsinki.fi>
2016-12-01 10:21:35 +0200

    Main toimii nyt

--- otmvk5/src/main/java/Main.java
+++ otmvk5/src/main/java/Main.java
@@ -8,6 +8,14 @@
  *
  * @author vpetteri
  */
-public class main {
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("testiteksti");
+    }
     
 }
